2a34576e05ac320a3af7166564d22a9f990d719d
alternistom <tocy777@gmail.com>
2024-07-18 12:54:48 +0200

    fix

--- assets/js/main_nobanner.js
+++ assets/js/main_nobanner.js
@@ -28,19 +28,7 @@
 		});
 
 	// Header.
-		if ($banner.length > 0
-		&&	$header.hasClass('alt')) {
-
-			$window.on('resize', function() { $window.trigger('scroll'); });
-
-			$banner.scrollex({
-				bottom:		$header.outerHeight(),
-				terminate:	function() { $header.removeClass('alt'); },
-				enter:		function() { $header.removeClass('alt'); },
-				leave:		function() { $header.removeClass('alt'); }
-			});
-
-		}
+	$header.addClass('alt');
 
 	// Menu.
 		var $menu = $('#menu');
